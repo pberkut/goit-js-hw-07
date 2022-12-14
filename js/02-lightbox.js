2926b9f9628a49110c90e4027e34cb33a0c2dfdd
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-14 14:01:34 +0200

    Completed Task # 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const markup = makeGalleryMarkup(galleryItems);
+
+function makeGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
