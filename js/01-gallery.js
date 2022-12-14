f1b0d422426bb6847bfbbc9df95867f26e444273
Pavlo Berkut <pavlo.berkut@gmail.com>
2022-12-14 12:17:40 +0200

    Completed Task # 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryMarkup();
+let instanceWindowModal;
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+galleryRef.addEventListener('click', onPictureClick);
+
+function galleryMarkup() {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    })
+    .join('');
+}
+
+function onPictureClick(evt) {
+  evt.preventDefault();
+  const isImg = evt.target.nodeName === 'IMG';
+  if (!isImg) {
+    return;
+  }
+
+  const pictureBigLink = evt.target.dataset.source;
+
+  openPictureInModalWindow(pictureBigLink);
+}
+
+function openPictureInModalWindow(src) {
+  instanceWindowModal = basicLightbox.create(
+    `
+        <img src="${src}" width="800" height="600">
+    `,
+    {
+      onShow: () => window.addEventListener('keydown', onPressEsc),
+      onClose: () => window.removeEventListener('keydown', onPressEsc),
+    },
+  );
+
+  instanceWindowModal.show();
+}
+
+function onPressEsc(evt) {
+  if (evt.code === 'Escape') {
+    instanceWindowModal.close();
+  }
+}
